909aad10f8723bd8b58fdf82a5f3afe751cfb591
Alberto Chan Fu <albertochanf@crimsonlogic.com>
2023-04-01 10:31:34 -0500

    project commit

---
+++ cleanarchitecture-chanteam/src/main/java/com/cursocrimson/cleanarchitecturechanteam/CleanarchitectureChanteamApplication.java
@@ -0,0 +1,15 @@
+package com.cursocrimson.cleanarchitecturechanteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchitectureChanteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchitectureChanteamApplication.class, args);
+	}
+
+}
+
+
---
+++ cleanarchitecture-chanteam/src/test/java/com/cursocrimson/cleanarchitecturechanteam/CleanarchitectureChanteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursocrimson.cleanarchitecturechanteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchitectureChanteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
